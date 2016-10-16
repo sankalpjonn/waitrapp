f81ba1c6a06e8ebda5fea33ec33e742db6f546d3
akash0675 <akashtalrejacs12@gmail.com>
2016-10-16 23:17:20 +0530

    qr code implementation

--- src/customer/components/MenuProduct/TableNoPopUp.js
+++ src/customer/components/MenuProduct/TableNoPopUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
-import Loader from './../Loader';
+import QrReader from 'react-qr-reader';
+// import Loader from './../Loader';
 import { saveOrderDetails } from './../../actions/OrderActions';
 import RestaurantStore from './../../stores/RestaurantStore';
 import OrderStore from './../../stores/OrderStore';
@@ -12,9 +13,24 @@ export default class TableNoPopUp extends React.Component {
 
     this.state = { isOrderInProgress: false };
 
-    this.onConfirmClick = this.onConfirmClick.bind(this);
+    // this.onConfirmClick = this.onConfirmClick.bind(this);
+    this.onHandleScan = this.onHandleScan.bind(this);
+    this.onHandleError = this.onHandleError.bind(this);
   }
 
+  onHandleScan(data) {
+    saveOrderDetails({
+      tableNumber: data,
+    });
+    window.setTimeout(() => {
+      this.context.router.push(`/restaurant/${RestaurantStore.getRestaurantID()}/orders`);
+    }, 3);
+  }
+
+  onHandleError(err) {
+    this.error = err;
+  }
+  /*
   onConfirmClick() {
     if (!this.tableNumber.value) {
       alert('Please enter a table number');
@@ -23,29 +39,44 @@ export default class TableNoPopUp extends React.Component {
 
     saveOrderDetails({
       tableNumber: this.tableNumber.value,
-      /* comments: this.comments.value, */
+      comments: this.comments.value,
     });
     window.setTimeout(() => {
       this.context.router.push(`/restaurant/${RestaurantStore.getRestaurantID()}/orders`);
     }, 3);
   }
-
+  */
   getDefaultTableNumber() {
     return OrderStore.getOrderDetails().tableNumber;
   }
 
   render() {
+    const previewStyle = {
+      height: '100%',
+      width: '100%',
+    };
+
     return (
       <div className={classnames('table-no-pop-up', { hide: !this.props.visibility })}>
         <div className="table-no-pop-up__form">
           <div className="form-group">
             <label>Table No</label>
+            <QrReader
+              previewStyle={previewStyle}
+              handleError={this.onHandleError}
+              handleScan={this.onHandleScan}
+              interval={100}
+              facingMode={'rear'}
+            />
+            <p>{this.error}</p>
+            {/*
             <input
               defaultValue={this.getDefaultTableNumber()}
               type="text"
               className="table-no-pop-up__form__table-no"
               ref={ref => { this.tableNumber = ref; }}
             />
+            */}
           </div>
           {/*
             <div className="form-group">
@@ -56,6 +87,7 @@ export default class TableNoPopUp extends React.Component {
               ></textarea>
             </div>
           */}
+          {/*
           <div className="table-no-pop-up__form__button">
             <button type="button" className="close" onClick={this.props.onClose}>Close</button>
             <button type="submit" className="confirm" onClick={this.onConfirmClick}>
@@ -66,6 +98,7 @@ export default class TableNoPopUp extends React.Component {
               }
             </button>
           </div>
+          */}
         </div>
       </div>
     );
